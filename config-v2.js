8e5f71578840b60f32e5760e4d875fb15ec16e53
NobBot2AF <eonobdggg12@gmail.com>
2024-09-22 12:21:42 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94787835062"
+const SESSION_ID = "PRABATH-MD~4CVSxbAC#6nNMlrO9mFTriQ6aeS-dcpptjNSMf7bR8powfC9zEuo"
 
 
 module.exports = {
